ab826f39eb52ae729b9a6ab68de4852697e28447
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-02-15 16:32:25 +0600

    done

--- app.js
+++ app.js
@@ -10,5 +10,27 @@ function updateProductCart(products, prices, isAdd) {
     productInput.value = inputUpdate;
     const getPrice = document.getElementById(products + '-total');
     getPrice.innerText = inputUpdate * prices
+    getSubTotal()
+}
 
+function calcSubTotal(products) {
+    const productsTotal = document.getElementById(products + '-number');
+    const getTotal = parseInt(productsTotal.value);
+    return getTotal;
+}
+
+function getSubTotal() {
+    const phoneTotal = calcSubTotal('phone') * 1219;
+    const casetotal = calcSubTotal('case') * 59;
+    const inTotal = phoneTotal + casetotal;
+    const tax = inTotal * 0.11;
+    const discount = inTotal * 0.05;
+    const subTotal = inTotal + tax - discount;
+    const totalPrice = subTotal
+    // console.log(inTotal);
+    document.getElementById('sub-total').innerText = inTotal;
+    document.getElementById('tax-amount').innerText = ` (+)  ${tax.toFixed(1)}`;
+    document.getElementById('discount-amount').innerText = `(-) ${discount.toFixed(1)}`;
+    document.getElementById('total-price').innerText = totalPrice.toFixed(2);
+    // return inTotal;
 }
